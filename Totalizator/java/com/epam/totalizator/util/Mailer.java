61713de87f243bb539557cd378099edac5bfa32d
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-10-10 22:54:48 +0300

    Add files via upload

---
+++ Totalizator/java/com/epam/totalizator/util/Mailer.java
@@ -0,0 +1,55 @@
+package com.epam.totalizator.util;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Mailer {
+	
+	private String userName;
+	private String password;
+	private Properties prop;
+	
+	public Mailer() {
+		this.userName = "atotalizator@gmail.com";
+		this.password = "Abcd-123";
+		
+		prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.port", "465");
+	}
+	
+	public void send(String subject, String text, String toEmail){
+        Session session = Session.getDefaultInstance(prop, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            //form 
+            message.setFrom(new InternetAddress(userName));
+            //to
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            //theme
+            message.setSubject(subject);
+            //body
+            message.setText(text);
+            //send message
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
